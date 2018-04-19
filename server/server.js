0337387955ac726eef36a845c2349660d02c61c4
Dmitriy001 <vasindmitriy1992@gmail.com>
2018-04-19 18:00:57 +0300

    last commit

--- server/server.js
+++ server/server.js
@@ -8,19 +8,28 @@ function randInt(min, max) {
     rand = Math.round(rand);
     return rand;
 }
-var fakeData = [{ id: 'Идентификатор', name: 'Название', price: 'Стоимость', quantity: 'Количество' }];
 
-
-
-
-for(var i=1; i<500; i++) {
-    fakeData.push([randInt(1,50), 'продукт ' + randInt(1, 99), randInt(100, 10000), randInt(1, 50)]);
+function generateData(length) {
+    var fakeData = [{ id: 'Идентификатор', name: 'Название', price: 'Стоимость', quantity: 'Количество' }];
+    for(var i=1; i<length; i++) {
+        fakeData.push([randInt(1,50), 'продукт ' + randInt(1, 99), randInt(100, 10000), randInt(1, 50)]);
+    }
+    return fakeData;
 }
 
 app.get('/data', function(req, res){
-    res.send(JSON.stringify(fakeData));
+    var id = req.query.id;
+    res.send(generateData(+id+1))
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/smalldata', function(req, res){
+
+    res.send(generateData(320));
+});
+app.get('/bigdata', function(req, res){
+    res.send(generateData(570));
+});
+
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!');
 });
